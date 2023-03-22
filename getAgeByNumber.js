8499fa457d995ce8d316d067a06c876e41261f7e
dzgierski19 <dzgierskigit@gmail.com>
2023-03-22 12:29:05 +0100

    task modified

--- getAgeByNumber.js
+++ getAgeByNumber.js
@@ -1,13 +1,34 @@
-const GET_AGE_BY_NUMBER = (number) => {
-  var age = null;
-
-  if (number < 10) {
-    age = "young";
-  } else if (number > 10 && number < 50) {
-    age = "adult";
-  } else {
-    age = "old";
-  }
-
-  console.log(age);
+const getAgeByNumber = (input) => {
+  return isNumberLessOrEqual10(input);
 };
+
+const isNumberLessOrEqual10 = (input) => {
+  isInputNumber(input);
+  isNumberHigherThanZero(input);
+  if (input < 10) {
+    return "young";
+  }
+  return isNumberLessThan50(input);
+};
+
+const isNumberLessThan50 = (input) => {
+  if (input < 50) {
+    return "adult";
+  }
+  return "old";
+};
+
+const isInputNumber = (input) => {
+  if (typeof input !== "number" && isNaN(input)) {
+    throw new Error("You must type a number");
+  }
+};
+
+const isNumberHigherThanZero = (input) => {
+  if (input <= 0) {
+    throw new Error("You must type number higher than 0");
+  }
+};
+
+const result1 = getAgeByNumber(1.5);
+console.log(result1);
